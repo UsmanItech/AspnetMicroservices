392bddaa147499f470346a105da3189adceeddd0
UsmanItech <grewaal549@gmail.com>
2024-11-03 18:17:25 +0500

    Baseket-api

---
+++ src/Services/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItems> Items { get; set; }
+
+        public ShoppingCart() { }
+        public ShoppingCart(string userName)
+        {
+            this.UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal total = 0;
+                foreach(var item in Items)
+                {
+                    total += item.Price * item.Quantity; 
+                }
+                return total;
+            }
+        }
+    }
+}
